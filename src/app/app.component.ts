d9603129b641b25a81cc8cd3be55a0faabe0fbeb
Anusha Babu <anushababu66@gmail.com>
2024-11-12 13:24:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import intlTelInput from 'intl-tel-input';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'flag';
+export class AppComponent implements OnInit {
+  title = 'intInputNew';
+  
+  ngOnInit(): void {
+    const inputElement = document.querySelector('#phone');
+    if (inputElement) {
+      const telInputInstance = intlTelInput(inputElement as HTMLInputElement, {
+        initialCountry: 'us',
+        separateDialCode: true,
+        utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js'
+      });
+
+      // Log the instance for debugging
+      console.log('IntlTelInput Instance:', telInputInstance);
+
+      // Example of attaching a country change event listener
+      inputElement.addEventListener('countrychange', () => {
+        const fullNumber = telInputInstance.getNumber();
+        console.log('Updated Full Phone Number:', fullNumber);
+      });
+    } else {
+      console.error('Phone input field not found!');
+    }
+  }
 }
